b6df9e7ea015b7c781c49dd0eaad9521a9694966
Mutegetsi prince <mutegetsi2000@gmail.com>
2022-02-17 21:38:54 +0200

    fibbo finished

--- testPrep/fibbo.cpp
+++ testPrep/fibbo.cpp
@@ -1,24 +1,18 @@
 #include <iostream>
 using namespace std;
-
-
 void isFibbo(int number){
-    int t1 = 1,t2=1 , nextTerm = 0;
+    int t1 = 1, t2 = 1 , nextTerm = 0;
     for(int i=1;i<number;i++){
-         if(i==1){
-             cout << t1 << ",";
-             continue;
-         }
          nextTerm = t1 + t2;
          t1 = t2;
          t2 = nextTerm;
-
-         cout << nextTerm << ", ";
+         if(nextTerm == number){
+             cout << nextTerm << " is a fibbon ";
+             break;
+         }         
     }
 }
-
 int main(){
-    isFibbo(12);
-
+    isFibbo(8);
     return 0;
 }
